bc469a5806cb29511b90bff8d45d43e6bb513868
yozi <j16412000kr@gmail.com>
2022-11-16 18:19:22 +0900

    pnpm workspace sample

---
+++ packages/app/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from '@kurly-frontend/common-components'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>컬리 모노레포 버튼은 잘 나오고 있는걸까요?</h1>
+      <div>
+        <Button />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ packages/common/src/components/Button.tsx
@@ -0,0 +1,5 @@
+const Button = () => {
+    return <button>@kurly-frontend/common Button Component</button>;
+};
+
+export default Button;
